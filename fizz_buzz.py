c7f490f906fa2e7df5b4738d9b82a3e8ad90d6ab
AljosaZ <znidaric.aljosa@gmail.com>
2020-04-23 10:25:24 +0200

    initial commit

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+print("Welcome to fizzbuzz counter!\n")
+
+count_limit = int(input("Please enter a number you want the counter to count up to: "))
+
+for i in range(1, count_limit +1):
+    if i%3 == 0 and i%5 ==0:
+        print("fizzbuzz")
+    elif i%3 == 0:
+        print("fizz")
+    elif i%5 == 0:
+        print("buzz")
+    else:
+        print(i)
